edb342bddd644630f4fbbb21d309a2f7cb49f591
klikar2115 <161980270+klikar2115@users.noreply.github.com>
2024-03-11 20:16:05 +0100

    Update init.js

--- init.js
+++ init.js
@@ -3,8 +3,8 @@
 $(document).ready(function() {
 						   
 	$(".countdown").countdown({
-				date: "11 mar 2024 22:00:00",
-				format: "off"
+				date: "12 mar 2024 22:00:00",
+				format: "on"
 			},
 			
 			function() {
